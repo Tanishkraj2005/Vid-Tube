ec253f4e89725cdf8d6c6b96ad08bdf516ae4e97
Tanishkraj2005 <rajtanishk475@gmail.com>
2026-04-23 11:28:16 +0530

    first commit

---
+++ src/Components/Recommended/Recommended.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Recommended.css'
+// import thumbnail1 from '../../assets/thumbnail1.png'
+
+const Recommended = () => {
+  return (
+    <div>Hey</div>
+  )
+}
+
+export default Recommended
