18abdbed5794489175703613b83b2c3bcb805427
Tanishkraj2005 <rajtanishk475@gmail.com>
2026-04-24 11:37:39 +0530

    added you-tube api and added content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from '../../Components/Sidebar/Sidebar'
 import './Home.css'
 import Feed from '../../Components/Feed/Feed'
 export const Home = ({sidebar}) => {
+
+  const [category,setCategory] = useState(0);
+
   return (
     <>
-      <Sidebar sidebar={sidebar} />
+      <Sidebar sidebar={sidebar} category={category} setCategory={setCategory} />
       <div className={`container ${sidebar?"":'large-container'}`}>
-        <Feed/>
+        <Feed category={category}/>
       </div>
     </>
   )
